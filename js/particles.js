ac142a87b1bf575011c4019b32696094d7df84d9
Echo <58097612+3kh0@users.noreply.github.com>
2022-01-08 18:00:36 -0500

    hello

--- js/particles.js
+++ js/particles.js
@@ -56,7 +56,7 @@ particlesJS("particles-js", {
     },
     "move": {
       "enable": true,
-      "speed": 6,
+      "speed": 4,
       "direction": "none",
       "random": true,
       "straight": false,
