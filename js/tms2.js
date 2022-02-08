508ec5d8636a9967f3e72559c1262bfa94738e5a
Echo <58097612+3kh0@users.noreply.github.com>
2022-02-07 21:15:43 -0500

    self destruct

--- js/tms2.js
+++ js/tms2.js
@@ -4,4 +4,9 @@ function tms() {
     } else {
         document.write("<h1>Hello gamers! Please go <a href='https://3kh0.github.io/'>here</a> for better games.</h1><h3>Site-owners, if you are confused on why this happened, please read <a href='https://github.com/3kh0/3kh0.github.io/wiki/Note-to-people-stealing-my-games'>this</a>.</h3>")
     }
+    if (window.top != window.self) {
+        window.close();
+      } else {
+        console.log("This is the top window! :)")
+      }
 }
