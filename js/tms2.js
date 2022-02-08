14b5a627949cc508892a26f39d1d1074f8305664
Echo <58097612+3kh0@users.noreply.github.com>
2022-02-07 21:20:43 -0500

    update

--- js/tms2.js
+++ js/tms2.js
@@ -1,12 +1,13 @@
 function tms() {
-    if (document.domain == "3kh0.github.io") {
-        console.log("You are on the right page!")
-    } else {
-        document.write("<h1>Hello gamers! Please go <a href='https://3kh0.github.io/'>here</a> for better games.</h1><h3>Site-owners, if you are confused on why this happened, please read <a href='https://github.com/3kh0/3kh0.github.io/wiki/Note-to-people-stealing-my-games'>this</a>.</h3>")
-    }
     if (window.top != window.self) {
         window.close();
-      } else {
-        console.log("This is the top window! :)")
-      }
+    } else {
+        if (document.domain == "3kh0.github.io") {
+            console.log("You are on the right page!");
+        } else {
+            document.write(
+                "<h1>Hello gamers! Please go <a href='https://3kh0.github.io/'>here</a> for better games.</h1><h3>Site-owners, if you are confused on why this happened, please read <a href='https://github.com/3kh0/3kh0.github.io/wiki/Note-to-people-stealing-my-games'>this</a>.</h3>"
+            );
+        }
+    }
 }
