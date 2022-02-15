2efc1694b69211264f3c00186bb4dc1c4b8454dc
Echo <58097612+3kh0@users.noreply.github.com>
2022-02-15 11:31:24 -0500

    trying doc.domain

--- js/tms2.js
+++ js/tms2.js
@@ -1,14 +1,6 @@
 function domaincheck() {
     if (document.domain == "3kh0.github.io") {
-        console.log("You are on the right page!"); // Show success
-        // check if page is in a iframe
-        var referrer = document.referrer;
-        if (referrer != "3kh0.github.io") {
-            // is in iframe and not on og domain
-            document.write("<h1>Hello gamers! Please go <a href='https://3kh0.github.io/'>here</a> for better games.</h1><h3>Site-owners, if you are confused on why this happened, please read <a href='https://github.com/3kh0/3kh0.github.io/wiki/Note-to-people-stealing-my-games'>this</a>.</h3>");
-        } else {
-            console.log("You are on the right page!"); // Show success
-        }
+        console.log("Correct! Game on!")
     } else {
         // not correct page
         document.write("<h1>Hello gamers! Please go <a href='https://3kh0.github.io/'>here</a> for better games.</h1><h3>Site-owners, if you are confused on why this happened, please read <a href='https://github.com/3kh0/3kh0.github.io/wiki/Note-to-people-stealing-my-games'>this</a>.</h3>");
@@ -16,6 +8,10 @@ function domaincheck() {
 }
 
 // Only for debug
+function msg() {
+    document.write("<h1>Hello gamers! Please go <a href='https://3kh0.github.io/'>here</a> for better games.</h1><h3>Site-owners, if you are confused on why this happened, please read <a href='https://github.com/3kh0/3kh0.github.io/wiki/Note-to-people-stealing-my-games'>this</a>.</h3>");
+    console.log("Done! :)")
+}
 function domaincheckF() {
     if (document.domain == "cheese.github.io") {
         console.log("You are on the right page!");
