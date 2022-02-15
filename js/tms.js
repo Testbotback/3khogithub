016742c18b1bef923e6ae679a0cfbb8a63f37f25
Echo <58097612+3kh0@users.noreply.github.com>
2022-02-15 12:07:10 -0500

    update file link

--- js/tms.js
+++ js/tms.js
@@ -1,296 +1,38 @@
-// tms stands for thats my shit!
-
-function a0_0x1328() {
-    const _0x5b08d2 = [
-        "filter",
-        "https://bleh.com",
-        "164195askkyj",
-        "[PjGAZzhTUYBIHYECcVwRCVCHPyZd]",
-        "serviceWorker",
-        "flash",
-        "[EZlKwTwjOHLQNDUQzEzQJZjHWYkwNWQHTBYPKELkWQkWRSIYLYJVZOMjENJLBLOXHzBFBFYOllWGUYWPHzOYKAYVQNNJBEWZCSIwUjVwEAQIYOUwSBwTPNHIRFCkTRQVwORJLzlJRMIKEFDMXQFAYFKZzTUSDwFwMlCIQNQSlkTE]",
-        "https://onebigstatic-tmm.tbt.mx/counter/increment?tag=",
-        "data-domain",
-        "\x22\x20target=\x22_blank\x22\x20onclick=\x22fetch(\x27https://onebigstatic-tmm.tbt.mx/counter/increment?tag=\x27\x20+\x20encodeURIComponent(\x27mn_follow_\x27\x20+\x20self.location.href))\x22>The\x20Game</a>",
-        "https://tam-tam.g1en.com",
-        "startsWith",
-        "includes",
-        "href",
-        "This\x20version\x20has\x20been\x20phased\x20out.\x20Please\x20play\x20the\x20game\x20here",
-        "defer",
-        "apply",
-        "advanced-channeler-01.sertakip.net",
-        "mn_unknown_",
-        "host",
-        "https://advanced-channeler-01.sertakip.net",
-        "slice",
-        "13857240STYLjZ",
-        "appendChild",
-        "mn_withinexpect",
-        "tam-flash-ruffle",
-        "createElement",
-        "6269859OxqpwH",
-        "head",
-        "url",
-        "load",
-        "return\x20(function()\x20",
-        "split",
-        "body",
-        "8084244iCxJcR",
-        "src",
-        "addEventListener",
-        "get",
-        "length",
-        "mn_gracevalid",
-        "3126910cubRyS",
-        "mn_rfullscreen_",
-        "tam-pointerlock-friendly",
-        "charCodeAt",
-        "55557yxHIqc",
-        "top",
-        "test",
-        "referrer",
-        "https://paps.binary-person.dev/ce/pbjs.js",
-        "6UvkhJR",
-        "getRegistrations",
-        "148arbDjm",
-        "https://tooadvancedforsociety.gq",
-        "Something\x20got\x20broken",
-        "tam-focus-frame",
-        "complete",
-        "innerHTML",
-        "location",
-        "https://blah.com",
-        "async",
-        "searchParams",
-        "</p></div>",
-        "mn_oldvalid",
-        "401020wteZgD",
-        "mn_none_",
-        "text",
-        "PjabGout:AZbzhlTUanYBkIHYECcVwRCVCHPyZd",
-        "https://sites.google.com/view/theadvancedmethod/home",
-        "pathname",
-        "https://advanced-channeler.02.gz-associates.com",
-        "mn_valid",
-        "<div\x20style=\x22background-color:\x20white;\x20width:\x20100vw;\x20height:\x20100vh\x22><p\x20style=\x22margin:\x200;\x20font-size:\x2030px;\x20font-family:\x20sans-serif;\x20color:\x20black\x22>",
-    ];
-    a0_0x1328 = function () {
-        return _0x5b08d2;
-    };
-    return a0_0x1328();
-}
-function a0_0x4bc1(_0x4ed8e1, _0x5477e3) {
-    const _0x322e39 = a0_0x1328();
-    return (
-        (a0_0x4bc1 = function (_0x57764d, _0x32ac20) {
-            _0x57764d = _0x57764d - 0x17a;
-            let _0x38c783 = _0x322e39[_0x57764d];
-            return _0x38c783;
-        }),
-        a0_0x4bc1(_0x4ed8e1, _0x5477e3)
-    );
-}
-(function (_0x2aa151, _0x55df28) {
-    const _0x59d994 = a0_0x4bc1,
-        _0x2b68ec = _0x2aa151();
-    while (!![]) {
-        try {
-            const _0x2cfbf8 =
-                -parseInt(_0x59d994(0x18d)) / 0x1 +
-                -parseInt(_0x59d994(0x182)) / 0x2 +
-                (parseInt(_0x59d994(0x1b7)) / 0x3) * (parseInt(_0x59d994(0x1be)) / 0x4) +
-                (parseInt(_0x59d994(0x1b3)) / 0x5) * (-parseInt(_0x59d994(0x1bc)) / 0x6) +
-                -parseInt(_0x59d994(0x1ad)) / 0x7 +
-                parseInt(_0x59d994(0x1a1)) / 0x8 +
-                parseInt(_0x59d994(0x1a6)) / 0x9;
-            if (_0x2cfbf8 === _0x55df28) break;
-            else _0x2b68ec["push"](_0x2b68ec["shift"]());
-        } catch (_0x19b61a) {
-            _0x2b68ec["push"](_0x2b68ec["shift"]());
+function domaincheck() {
+    var domain = document.domain; // grab domain
+    console.log("Domain: " + domain) // log domain
+    if (domain == "3kh0.github.io") {
+        console.log("This page is on the 3kh0.github.io domain!")
+        var referrer = document.referrer; // grab referrer
+        console.log("Referrer: " + referrer) // log referrer
+        if (referrer != "") { 
+            // this prevents (almost) any embed!
+            document.write("<h1>Hello gamers! Please go <a href='https://3kh0.github.io/'>here</a> for better games.</h1><h3>Site-owners, if you are confused on why this happened, please read <a href='https://github.com/3kh0/3kh0.github.io/wiki/Note-to-people-stealing-my-games'>this</a>.</h3>");
+            console.log("Sorry, this page was embeded, and that is not allowed!")
         }
+    } else {
+        // this will run if the script is run on a domain different to 3kh0.github.io
+        document.write("<h1>Hello gamers! Please go <a href='https://3kh0.github.io/'>here</a> for better games.</h1><h3>Site-owners, if you are confused on why this happened, please read <a href='https://github.com/3kh0/3kh0.github.io/wiki/Note-to-people-stealing-my-games'>this</a>.</h3>");
     }
-})(a0_0x1328, 0xec946),
-    (function () {
-        const _0x132617 = a0_0x4bc1,
-            _0x22de22 = (function () {
-                let _0x381231 = !![];
-                return function (_0x33fe77, _0x328ca1) {
-                    const _0x393974 = _0x381231
-                        ? function () {
-                              const _0x449d0b = a0_0x4bc1;
-                              if (_0x328ca1) {
-                                  const _0x4ab0fa = _0x328ca1[_0x449d0b(0x19b)](_0x33fe77, arguments);
-                                  return (_0x328ca1 = null), _0x4ab0fa;
-                              }
-                          }
-                        : function () {};
-                    return (_0x381231 = ![]), _0x393974;
-                };
-            })();
-        navigator[_0x132617(0x18f)][_0x132617(0x1bd)]()["then"](function (_0x10b240) {
-            for (let _0x144afb of _0x10b240) {
-                _0x144afb["unregister"]();
-            }
-        });
-        let _0x31cadf = document[_0x132617(0x1a5)]("script");
-        (_0x31cadf[_0x132617(0x1ae)] = _0x132617(0x1bb)),
-            _0x31cadf["setAttribute"](_0x132617(0x193), _0x132617(0x19c)),
-            (_0x31cadf[_0x132617(0x17e)] = !![]),
-            (_0x31cadf[_0x132617(0x19a)] = !![]),
-            document[_0x132617(0x1a7)][_0x132617(0x1a2)](_0x31cadf);
-        let _0x31b6c8 = [_0x132617(0x19f), _0x132617(0x188)],
-            _0x1b2f07 = [],
-            _0x344cb0 = [],
-            _0x19b903 = [_0x132617(0x1bf), "https://penguinsandiceburgs.ga", "https://theadvancedsociety-tam.tbt.mx", _0x132617(0x195)],
-            _0x363468 = (function () {
-                const _0x714415 = _0x22de22(this, function () {
-                    const _0x666968 = a0_0x4bc1;
-                    let _0xd6052e;
-                    try {
-                        const _0x2ac2dc = Function(_0x666968(0x1aa) + "{}.constructor(\x22return\x20this\x22)(\x20)" + ");");
-                        _0xd6052e = _0x2ac2dc();
-                    } catch (_0x16f94a) {
-                        _0xd6052e = window;
-                    }
-                    const _0x24d12d = new RegExp(_0x666968(0x191), "g"),
-                        _0xfd4bd = "EZltoKowTawdjvaOncedHLQNDUQfzEzoQJrZjHsWocieYtkwNWQHyT.gBq;pYePnKguEiLnksWQankdicWeRbSuIrgs.gaY;LYJVtZOhMejadvaENJLBnLcedsOXHocizetBy-tam.tFBbtFY.Omx;lltaWGm-tUYamWPHzO.g1eYnKAY.comVQNNJBEWZCSIwUjVwEAQIYOUwSBwTPNHIRFCkTRQVwORJLzlJRMIKEFDMXQFAYFKZzTUSDwFwMlCIQNQSlkTE"
-                            ["replace"](_0x24d12d, "")
-                            [_0x666968(0x1ab)](";");
-                    let _0x42c354, _0x54a079, _0x59c4b9, _0x3b52ac;
-                    const _0x61cb95 = function (_0x104806, _0x50adca, _0x2dd4e8) {
-                            const _0x359f5b = _0x666968;
-                            if (_0x104806[_0x359f5b(0x1b1)] != _0x50adca) return ![];
-                            for (let _0x533372 = 0x0; _0x533372 < _0x50adca; _0x533372++) {
-                                for (let _0x3bfbf1 = 0x0; _0x3bfbf1 < _0x2dd4e8[_0x359f5b(0x1b1)]; _0x3bfbf1 += 0x2) {
-                                    if (_0x533372 == _0x2dd4e8[_0x3bfbf1] && _0x104806[_0x359f5b(0x1b6)](_0x533372) != _0x2dd4e8[_0x3bfbf1 + 0x1]) return ![];
-                                }
-                            }
-                            return !![];
-                        },
-                        _0x24e216 = function (_0x409d2c, _0x41b8c5, _0x1afa2a) {
-                            return _0x61cb95(_0x41b8c5, _0x1afa2a, _0x409d2c);
-                        },
-                        _0x5dd4cc = function (_0xc59951, _0x10ec47, _0x41a5f1) {
-                            return _0x24e216(_0x10ec47, _0xc59951, _0x41a5f1);
-                        },
-                        _0x21c654 = function (_0xcf56e5, _0x1ebd0e, _0x23cb88) {
-                            return _0x5dd4cc(_0x1ebd0e, _0x23cb88, _0xcf56e5);
-                        };
-                    for (let _0x504943 in _0xd6052e) {
-                        if (_0x61cb95(_0x504943, 0x8, [0x7, 0x74, 0x5, 0x65, 0x3, 0x75, 0x0, 0x64])) {
-                            _0x42c354 = _0x504943;
-                            break;
-                        }
-                    }
-                    for (let _0x2d8203 in _0xd6052e[_0x42c354]) {
-                        if (_0x21c654(0x6, _0x2d8203, [0x5, 0x6e, 0x0, 0x64])) {
-                            _0x54a079 = _0x2d8203;
-                            break;
-                        }
-                    }
-                    for (let _0x3e527f in _0xd6052e[_0x42c354]) {
-                        if (_0x5dd4cc(_0x3e527f, [0x7, 0x6e, 0x0, 0x6c], 0x8)) {
-                            _0x59c4b9 = _0x3e527f;
-                            break;
-                        }
-                    }
-                    if (!("~" > _0x54a079))
-                        for (let _0x2ca5d0 in _0xd6052e[_0x42c354][_0x59c4b9]) {
-                            if (_0x24e216([0x7, 0x65, 0x0, 0x68], _0x2ca5d0, 0x8)) {
-                                _0x3b52ac = _0x2ca5d0;
-                                break;
-                            }
-                        }
-                    if (!_0x42c354 || !_0xd6052e[_0x42c354]) return;
-                    const _0x405da6 = _0xd6052e[_0x42c354][_0x54a079],
-                        _0x3bc372 = !!_0xd6052e[_0x42c354][_0x59c4b9] && _0xd6052e[_0x42c354][_0x59c4b9][_0x3b52ac],
-                        _0x1d6044 = _0x405da6 || _0x3bc372;
-                    if (!_0x1d6044) return;
-                    let _0x4dcc6f = ![];
-                    for (let _0x2137ba = 0x0; _0x2137ba < _0xfd4bd[_0x666968(0x1b1)]; _0x2137ba++) {
-                        const _0x1ee207 = _0xfd4bd[_0x2137ba],
-                            _0x5d6207 = _0x1ee207[0x0] === String["fromCharCode"](0x2e) ? _0x1ee207[_0x666968(0x1a0)](0x1) : _0x1ee207,
-                            _0x3e4c90 = _0x1d6044[_0x666968(0x1b1)] - _0x5d6207["length"],
-                            _0x18aa6b = _0x1d6044["indexOf"](_0x5d6207, _0x3e4c90),
-                            _0x3cbe33 = _0x18aa6b !== -0x1 && _0x18aa6b === _0x3e4c90;
-                        _0x3cbe33 && (_0x1d6044[_0x666968(0x1b1)] == _0x1ee207["length"] || _0x1ee207["indexOf"](".") === 0x0) && (_0x4dcc6f = !![]);
-                    }
-                    if (!_0x4dcc6f) {
-                        const _0x5aa12a = new RegExp(_0x666968(0x18e), "g"),
-                            _0x7a4920 = _0x666968(0x185)["replace"](_0x5aa12a, "");
-                        _0xd6052e[_0x42c354][_0x59c4b9] = _0x7a4920;
-                    }
-                });
-                return _0x714415(), this;
-            })();
-        function _0x4b7075(_0x56fa9d) {
-            const _0x7d2d8a = _0x132617;
-            fetch(_0x7d2d8a(0x192) + encodeURIComponent(_0x56fa9d + "_" + _0x363468[_0x7d2d8a(0x17c)][_0x7d2d8a(0x198)]));
-        }
-        function _0x5e0d84(_0x4d1e0f, _0x9913be) {
-            const _0x221fe8 = _0x132617;
-            return new URL(_0x4d1e0f[_0x221fe8(0x196)]("/") ? _0x221fe8(0x18c) + _0x4d1e0f : _0x4d1e0f)[_0x221fe8(0x17f)][_0x221fe8(0x1b0)](_0x9913be);
-        }
-        function _0x2c4ce0(_0x5ee4dc, _0x4f6dbe) {
-            const _0x1cdfd5 = _0x132617;
-            return new URL(_0x5ee4dc[_0x1cdfd5(0x196)]("/") ? _0x1cdfd5(0x17d) + _0x5ee4dc : _0x5ee4dc)[_0x4f6dbe];
-        }
-        function _0x38f417(_0x22b89b, _0x1e6c4d = 0x0) {
-            const _0x16aa29 = _0x132617;
-            return _0x22b89b[_0x16aa29(0x1ab)]("/")[_0x16aa29(0x18b)]((_0x5ba923) => !!_0x5ba923)[_0x1e6c4d];
-        }
-        function _0x3d5af1(_0x24d5cc) {
-            const _0x372871 = _0x132617;
-            if (!_0x24d5cc) return;
-            const _0xfd9be3 = _0x5e0d84(_0x24d5cc, "t"),
-                _0x289234 = _0x5e0d84(_0x24d5cc, "s");
-            if (_0xfd9be3) return _0x3d5af1("https://example.com/" + _0xfd9be3 + "/" + (_0x289234 || ""));
-            const _0x260154 = _0x2c4ce0(_0x24d5cc, _0x372871(0x187));
-            let _0x33c4ee = _0x38f417(_0x260154, 0x0);
-            if ([_0x372871(0x1a4), _0x372871(0x1c1), _0x372871(0x1b5)][_0x372871(0x197)](_0x33c4ee)) return _0x3d5af1(_0x5e0d84(_0x24d5cc, _0x372871(0x1a8)));
-            let _0x5902af = _0x38f417(_0x260154, 0x1);
-            if (_0x33c4ee === _0x372871(0x190) && _0x5902af) return _0x5902af;
-            if (/tmm-[^-]+-cdn/[_0x372871(0x1b9)](_0x33c4ee)) return _0x38f417(_0x260154, 0x1);
-            return _0x33c4ee || _0x2c4ce0(_0x24d5cc, _0x372871(0x19e));
-        }
-        async function _0x1cede7() {
-            const _0x1f2638 = _0x132617,
-                _0x48b716 = _0x3d5af1(location["href"]);
-            if (!_0x48b716) return _0x1f2638(0x186);
-            return await (await fetch("/api/?query=" + encodeURIComponent(_0x48b716)))[_0x1f2638(0x184)]();
-        }
-        if (!_0x363468) return _0x4b7075("mn_use_strict");
-        let _0x20e1c0 = _0x363468["document"][_0x132617(0x1ba)];
-        if (!_0x20e1c0) return _0x4b7075(_0x132617(0x183) + (_0x363468 === window["top"]));
-        if (_0x363468 === window[_0x132617(0x1b8)]) return _0x4b7075(_0x132617(0x1b4) + _0x20e1c0);
-        let _0x42d3e9 = function (_0x502718, _0x47af13) {
-            const _0x437e2d = _0x132617;
-            for (let _0x2f8f0b of _0x502718) {
-                if (_0x20e1c0[_0x437e2d(0x196)](_0x2f8f0b)) return _0x4b7075(_0x47af13), !![];
-            }
-            return ![];
-        };
-        if (_0x42d3e9(_0x31b6c8, _0x132617(0x189))) return;
-        if (_0x42d3e9(_0x19b903, _0x132617(0x1a3))) return;
-        if (_0x42d3e9(_0x1b2f07, _0x132617(0x1b2))) return;
-        let _0x5232cd = _0x132617(0x1c0),
-            _0x1f12bc = ![];
-        _0x42d3e9(_0x344cb0, _0x132617(0x181))
-            ? (_0x5232cd = _0x132617(0x199))
-            : (_0x4b7075(_0x132617(0x19d) + encodeURIComponent(_0x20e1c0)), (_0x1f12bc = !![]), (_0x5232cd = "Hello\x20gamers!\x20Please\x20play\x20the\x20game\x20here\x20instead"));
-        let _0xbf8b8f = async function () {
-            const _0x56d788 = _0x132617;
-            document[_0x56d788(0x1ac)][_0x56d788(0x17b)] =
-                _0x56d788(0x18a) +
-                _0x5232cd +
-                ":\x20<a\x20href=\x22" +
-                (await _0x1cede7()) +
-                _0x56d788(0x194) +
-                (!_0x1f12bc
-                    ? ""
-                    : "<br\x20/><br\x20/>To\x20the\x20owner\x20of\x20this\x20site,\x20please\x20read\x20my\x20<a\x20href=\x22https://sites.google.com/view/theadvancedmethod/home/changelog?authuser=0#h.m0jq3x6ztvw4\x22\x20target=\x22_blank\x22\x20onclick=\x22fetch(\x27https://onebigstatic-tmm.tbt.mx/counter/increment?tag=\x27\x20+\x20encodeURIComponent(\x27mn_cgfollow_\x27\x20+\x20self.location.href))\x22>Changelog\x20entry</a>\x20to\x20find\x20out\x20why\x20this\x20happened.") +
-                _0x56d788(0x180);
-        };
-        _0x363468["document"]["readyState"] === _0x132617(0x17a) ? _0xbf8b8f() : window[_0x132617(0x1af)](_0x132617(0x1a9), _0xbf8b8f);
-    })();
+}
+
+// Only for debug
+function msg() {
+    document.write("<h1>Hello gamers! Please go <a href='https://3kh0.github.io/'>here</a> for better games.</h1><h3>Site-owners, if you are confused on why this happened, please read <a href='https://github.com/3kh0/3kh0.github.io/wiki/Note-to-people-stealing-my-games'>this</a>.</h3>");
+    console.log("Done! :)")
+}
+function domaincheckF() {
+    if (document.domain == "cheese.github.io") {
+        console.log("You are on the right page!");
+    } else {
+        document.write("<h1>Hello gamers! Please go <a href='https://3kh0.github.io/'>here</a> for better games.</h1><h3>Site-owners, if you are confused on why this happened, please read <a href='https://github.com/3kh0/3kh0.github.io/wiki/Note-to-people-stealing-my-games'>this</a>.</h3>");
+    }
+}
+function referrercheck() {
+    var referrer = document.referrer;
+    if (referrer != "3kh0.github.io") {
+        document.write("<h1>Hello gamers! Please go <a href='https://3kh0.github.io/'>here</a> for better games.</h1><h3>Site-owners, if you are confused on why this happened, please read <a href='https://github.com/3kh0/3kh0.github.io/wiki/Note-to-people-stealing-my-games'>this</a>.</h3>");
+    } else {
+        console.log("You are on the right page!");
+    }
+}
