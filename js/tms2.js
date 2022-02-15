c84b5d04dfeea3c8bdf442b56b2aab4de609db57
Echo <58097612+3kh0@users.noreply.github.com>
2022-02-15 11:46:29 -0500

    log vars

--- js/tms2.js
+++ js/tms2.js
@@ -1,8 +1,10 @@
 function domaincheck() {
     var domain = document.domain;
+    console.log(domain)
     if (domain == "3kh0.github.io") {
         console.log("This page is on the 3kh0.github.io domain!")
         var referrer = document.referrer; // grab referrer
+        console.log(referrer) // log referrer
         if (referrer != "null") { 
             // this prevents (almost) any embed!
             document.write("<h1>Hello gamers! Please go <a href='https://3kh0.github.io/'>here</a> for better games.</h1><h3>Site-owners, if you are confused on why this happened, please read <a href='https://github.com/3kh0/3kh0.github.io/wiki/Note-to-people-stealing-my-games'>this</a>.</h3>");
