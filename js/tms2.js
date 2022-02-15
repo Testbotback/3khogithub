57c8e401e07ce5ae3534679e3e8c53bf16654943
Echo <58097612+3kh0@users.noreply.github.com>
2022-02-15 11:04:52 -0500

    blank

--- js/tms2.js
+++ js/tms2.js
@@ -16,7 +16,7 @@ function referrercheck() {
     var referrer = document.referrer;
     if (referrer != "3kh0.github.io") {
         document.write("<h1>Hello gamers! Please go <a href='https://3kh0.github.io/'>here</a> for better games.</h1><h3>Site-owners, if you are confused on why this happened, please read <a href='https://github.com/3kh0/3kh0.github.io/wiki/Note-to-people-stealing-my-games'>this</a>.</h3>");
-    } else {
+    } if (referrer == "") {
         console.log("You are on the right page!");
     }
 }
