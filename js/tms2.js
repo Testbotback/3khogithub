295f0065fd14161b5735cc6e6a1131f248ca9af1
Echo <58097612+3kh0@users.noreply.github.com>
2022-02-15 11:25:55 -0500

    trying to make sense of everything

--- js/tms2.js
+++ js/tms2.js
@@ -1,10 +1,21 @@
 function domaincheck() {
     if (document.domain == "3kh0.github.io") {
-        console.log("You are on the right page!");
+        console.log("You are on the right page!"); // Show success
+        // check if page is in a iframe
+        var referrer = document.referrer;
+        if (referrer != "3kh0.github.io") {
+            // is in iframe and not on og domain
+            document.write("<h1>Hello gamers! Please go <a href='https://3kh0.github.io/'>here</a> for better games.</h1><h3>Site-owners, if you are confused on why this happened, please read <a href='https://github.com/3kh0/3kh0.github.io/wiki/Note-to-people-stealing-my-games'>this</a>.</h3>");
+        } else {
+            console.log("You are on the right page!"); // Show success
+        }
     } else {
+        // not correct page
         document.write("<h1>Hello gamers! Please go <a href='https://3kh0.github.io/'>here</a> for better games.</h1><h3>Site-owners, if you are confused on why this happened, please read <a href='https://github.com/3kh0/3kh0.github.io/wiki/Note-to-people-stealing-my-games'>this</a>.</h3>");
     }
 }
+
+// Only for debug
 function domaincheckF() {
     if (document.domain == "cheese.github.io") {
         console.log("You are on the right page!");
