4bad18b3308479d05354b37e3e9531091cbf7d14
Echo <58097612+3kh0@users.noreply.github.com>
2022-02-16 12:06:17 -0500

    Update tms.js

--- js/tms.js
+++ js/tms.js
@@ -22,7 +22,7 @@ function check() {
 // These functions can be used for debug purposes
 function msg() {
     document.write("<h1>Hello gamers! Please go <a href='https://3kh0.github.io/'>here</a> for better games.</h1><h3>Site-owners, if you are confused on why this happened, please read <a href='https://github.com/3kh0/3kh0.github.io/wiki/Note-to-people-stealing-my-games'>this</a>.</h3>");
-    console.log("Done! :)")
+    console.log("Sucess! :)")
 }
 function getReferrer() {
     var referrer = document.referrer; // grab referrer
