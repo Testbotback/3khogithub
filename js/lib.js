e74a3683823707cb26a546f1a4e58b35251c52f7
Echo <58097612+3kh0@users.noreply.github.com>
2022-02-16 12:39:00 -0500

    change domain to vercel domain.

--- js/lib.js
+++ js/lib.js
@@ -2,7 +2,7 @@ function check() {
     console.log("Checking this page... Please wait.")
     var domain = document.domain; // grab domain
     console.log("Domain: " + domain) // log domain
-    if (domain == "3kh0.github.io") {
+    if (domain == "3kh0-git-tms-3kh0.vercel.app") {
         console.log("This page is on the 3kh0.github.io domain!")
         var referrer = document.referrer; // grab referrer
         console.log("Referrer: " + referrer) // log referrer
@@ -15,6 +15,7 @@ function check() {
         // this will run if the script is run on a domain different to 3kh0.github.io
         document.write("<h1>Hello gamers! Please go <a href='https://3kh0.github.io/'>here</a> for better games.</h1><h3>Site-owners, if you are confused on why this happened, please read <a href='https://github.com/3kh0/3kh0.github.io/wiki/Note-to-people-stealing-my-games'>this</a>.</h3><p>Was this site supposed to work? <a href='https://github.com/3kh0/3kh0.github.io/issues'>Please report it</a>!</p>");
         console.log("Sorry, but this code is copyrighted so you can not just yoink it! Want to have this game on your site? Read this: https://github.com/3kh0/3kh0.github.io/wiki/Note-to-people-stealing-my-games")
+        console.log("Domain error")
     }
     console.log("The check is complete! You can use check(); to run it again if you wish.")
 }
